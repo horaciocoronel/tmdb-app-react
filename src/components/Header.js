d1e4b0ecd1656dd134fc453f0c27d2d37388636c
horaciocoronel <arielc@gmail.com>
2018-03-14 00:21:01 -0400

    Add Header component with styling

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,18 +1,15 @@
 import React, { Component } from 'react';
-import logo from '../assets/media/logo.svg';
+import logo from '../assets/media/tmdb-logo-green.svg';
 
 class Header extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
+        <header className="ug-row">
+					<div className="ug-col-12">
+	          <img src={logo} alt="logo" />
+						<h1 className="menu-brand">TMDB App React</h1>
+					</div>
         </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
     );
   }
 }
