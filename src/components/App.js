6c5c5f683c8c180dfb67bd6ddcc113c6662e2a11
horaciocoronel <arielc@gmail.com>
2018-03-14 00:23:00 -0400

    Add MainContent Component

--- src/components/App.js
+++ src/components/App.js
@@ -1,12 +1,40 @@
 import React, { Component } from 'react';
 import Header from './Header';
-import '../assets/styles/simple-grid.css';
+import MainContent from './MainContent';
+import '../assets/styles/ug-grid.css';
 import '../assets/styles/App.css';
 
 class App extends Component {
+	constructor(props) {
+		super(props);
+		this.state = {
+			films: []
+		}
+	}
+
+	componentDidMount() {
+			// Call the API and set the Initial list
+			const url = `https://api.themoviedb.org/3/movie/upcoming?api_key=${process.env.REACT_APP_TMDB_KEY}&language=en-CA&page=1`;
+			fetch(`${url}`)
+	      .then(res => res.json())
+	      .then(
+	        (result) => {
+	          this.setState({
+	            films: result.results
+	          });
+	        },
+	        (error) => {
+	          alert("Couldn't reach the API server")
+	        }
+	      )
+  }
+
   render() {
     return (
-      <Header />
+			<div className="ug-wrapper">
+	      	<Header />
+					<MainContent films={this.state.films} />
+			</div>
     );
   }
 }
---
+++ src/components/MainContent.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+
+class MainContent extends Component {
+  render() {
+    return (
+			<div className="ug-wrapper">
+			</div>
+    );
+  }
+}
+
+export default MainContent;
