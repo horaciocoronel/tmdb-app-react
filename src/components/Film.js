8b804f69c35d417d53cbd7066ab7db9437761a1c
horaciocoronel <arielc@gmail.com>
2018-03-14 00:45:51 -0400

    Display films poster in MainContent component

--- src/components/Film.js
+++ src/components/Film.js
@@ -2,8 +2,10 @@ import React, { Component } from 'react';
 
 class Film extends Component {
   render() {
+		const posterPathBase = ' http://image.tmdb.org/t/p/w300';
     return (
-				<div class="ug-col-s-6 ug-col-m-4 ug-col-l-2"  key={this.props.film.id}>
+				<div className="ug-col-s-6 ug-col-m-4 ug-col-l-2"  key={this.props.film.id}>
+					<img className="poster-image" src={posterPathBase+this.props.film.poster_path} alt={this.props.film.title} />
 					<h3>{this.props.film.title}</h3>
         </div>
     );
