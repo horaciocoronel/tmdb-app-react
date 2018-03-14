d3d7208995b8ec02e37d379f42aafcb8d453b38e
horaciocoronel <arielc@gmail.com>
2018-03-14 01:59:52 -0400

    Move image tag to background image to avoid gap between images when height ain't the same

--- src/components/Film.js
+++ src/components/Film.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 
 class Film extends Component {
-	
+
 	handleDetailsClick = (e, film) => { this.props.currentFilm() }
 
   render() {
@@ -9,13 +9,11 @@ class Film extends Component {
     return (
 
 				<div
-					className="ug-col-s-6 ug-col-m-4 ug-col-l-3 film-poster"
+					className="ug-col-s-6 ug-col-m-4 ug-col-l-3 film-poster film-poster-img"
 					key={this.props.film.id}
 					onClick={(e) => this.handleDetailsClick(e, this.props.film)}
+					style={{backgroundImage: `url(${posterPathBase+this.props.film.poster_path})`}}
 					>
-					<img
-						src={posterPathBase+this.props.film.poster_path}
-						alt={this.props.film.title} />
         </div>
     );
   }
